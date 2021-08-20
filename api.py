cbca85e0bdf7322393f4e1229eeb98c931596368
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-08-20 12:46:59 +0900

    replace

---
+++ api.py
@@ -0,0 +1,116 @@
+from flask import redirect, request, render_template, jsonify, Blueprint, session, g
+from models import User, Post
+from db_connect import db
+from flask_bcrypt import Bcrypt
+
+board = Blueprint('board', __name__)
+bcrypt = Bcrypt()
+
+
+@board.before_app_request
+def load_logged_in_user():
+    user_id = session.get('login')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = db.session.query(User).filter(User.id == user_id).first()
+
+
+@board.route("/")
+def home():
+    if session.get("login") is None:
+        return redirect("/login")
+    else:
+        return redirect("/post")
+
+
+@board.route("/post", methods=["GET"])
+def post():
+    if session.get("login") is not None:
+        if request.method == 'GET':
+            data = Post.query.order_by(Post.created_at.desc()).all()
+            return render_template("index.html", post_list=data)
+        else:
+            content = request.form['content']
+            author = request.form['author']
+
+            post = Post(author, content)
+            db.session.add(post)
+            db.session.commit()
+            return jsonify({"result": "success"})
+    else:
+        return redirect("/")
+
+
+@board.route("/join", methods=['GET', 'POST'])
+def join():
+    if session.get("login") is None:
+        if request.method == 'GET':
+            return render_template("join.html")
+        else:
+            user_id = request.form['user_id']
+            user_pw = request.form['user_pw']
+
+            pw_hash = bcrypt.generate_password_hash(user_pw)
+
+            user = User(user_id, pw_hash)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"result": "success"})
+    else:
+        return redirect("/")
+
+
+@board.route("/login", methods=['GET', 'POST'])
+def login():
+    if session.get("login") is None:
+        if request.method == 'GET':
+            return render_template("login.html")
+        else:
+            user_id = request.form['user_id']
+            user_pw = request.form['user_pw']
+
+            user = User.query.filter(User.user_id == user_id).first()
+
+            if user is not None:
+                if bcrypt.check_password_hash(user.user_pw, user_pw):
+                    session['login'] = user.id
+                    return jsonify({"result": "success"})
+                else:
+                    return jsonify({"result": "fail"})
+            else:
+                return jsonify({"result": "fail"})
+    else:
+        return redirect("/")
+
+
+@board.route("/logout")
+def logout():
+    session['login'] = None
+    return redirect("/")
+
+
+@board.route("/post", methods=["DELETE"])
+def delete_post():
+    id = request.form['id']
+    author = request.form['author']
+    data = Post.query.filter(Post.id == id, Post.author == author).first()
+    if data is not None:
+        db.session.delete(data)
+        db.session.commit()
+        return jsonify({"result": "success"})
+    else:
+        return jsonify({"result": "fail"})
+
+
+@board.route("/post", methods=["PATCH"])
+def update_post():
+    id = request.form['id']
+    content = request.form['content']
+    author = User.query.filter(User.id == session['login']).first()
+    data = Post.query.filter(
+        Post.id == id, Post.author == author.user_id).first()
+
+    data.content = content
+    db.session.commit()
+    return jsonify({"result": "success"})
--- app.py
+++ app.py
@@ -1,67 +1,18 @@
 import pymysql
-from flask import Flask, request, render_template, session, url_for, redirect, jsonify, Blueprint
+from flask import Flask, render_template
+from api import board
 from db_connect import db
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.register_blueprint(board)
 
-# app.secret_key = 'super secret key'
-# app.config['SESSION_TYPE'] = 'filesystem'
-# userinfo = {'Elice': '1q2w3e4r!!'}
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:devpass@127.0.0.1:3306/elice_flask_board"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'asodfajsdofijac'
 
 db.init_app(app)
-
-
-@app.route("/")
-def home():
-    if session.get('logged_in'):
-        return render_template('loggedin.html')
-    else:
-        return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        name = request.form['username']
-        password = request.form['password']
-        try:
-            if name in userinfo:
-                # 비밀번호 검증 후 일치하는 경우 초기 페이지로 이동하세요.
-                if userinfo[name] == password:
-                    session['logged_in'] = True
-                    return redirect(url_for('home'))
-                else:
-                    return '비밀번호가 틀립니다.'
-
-            return '아이디가 없습니다.'  # try의 return
-        except:
-            return 'Do not login'
-    else:
-        return render_template('login.html')
-
-
-@app.route('/join', methods=['GET', 'POST'])
-def join():
-    if request.method == 'GET':
-        return render_template("register.html")
-    else:
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
-
-        pw_hash = bcrypt.generate_password_hash(user_pw)
-
-        user = User(user_id, pw_hash)
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({"result": "success"})
-
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False  # 또는 None 으로 값이 없음을 표현함
-    return render_template('index.html')
-
+bcrypt = Bcrypt(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
