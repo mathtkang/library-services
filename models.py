4c459fd6d335c15dc203bceab22224aaa374b674
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-08-23 19:16:02 +0900

    api작성

--- models.py
+++ models.py
@@ -5,12 +5,13 @@ from datetime import datetime
 [데이터베이스 제약 조건 명시 파일]
 관계형 데이터베이스의 데이터를 객체랑 연결 시켜주는 것을 ORM (Object Relational Mapping)이라 한다.
 즉, 외부에 존재하는 DB를 서버에서 사용하기 위해, DB와 동일한 제약조건을 객체에 부여하는 파일이다.
-
+테이블명 : 객체 이름과 달라도 되지만, 외부 테이블에서 호출시 이름으로 사용(소문자로 시작)
 '''
 
 
+# 도서관 유저 테이블
 class LibraryUser(db.Model):
-    # 테이블명 : 객체 이름과 달라도 되지만, 외부 테이블에서 호출시 이름으로 사용(소문자로 시작)
+
     __tablename__ = 'libraryUser'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -24,57 +25,59 @@ class LibraryUser(db.Model):
         self.password = password
 
 
+# 전체 책 목록 테이블
 class LibraryBook(db.Model):
-    # 책 소개 데이터
+
     __tablename__ = 'libraryBook'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    book_name = db.Column(db.String(255), nullable=False)
-    publisher = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
-    publication_date = db.Column(db.Date, nullable=False)
-    pages = db.Column(db.Integer, nullable=False)
-    isbn = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.Text(), nullable=False)
-    star = db.Column(db.Integer, nullable=False)
-    img_link = db.Column(db.String(255), nullable=False)
-    remaining = db.Column(db.Integer, nullable=False)
-    # rental_val = db.Column(db.Integer, nullable=False)
+    book_name = db.Column(db.String(255), nullable=False)  # 책이름
+    publisher = db.Column(db.String(255), nullable=False)  # 출판사
+    author = db.Column(db.String(255), nullable=False)  # 저자
+    publication_date = db.Column(db.Date, nullable=False)  # 출간일
+    pages = db.Column(db.Integer, nullable=False)  # 페이지 수
+    isbn = db.Column(db.Integer, nullable=False)  # ISBN 코드
+    description = db.Column(db.Text(), nullable=False)  # 책 소개
+    star = db.Column(db.Integer, nullable=False)  # 별점
+    img_link = db.Column(db.String(255), nullable=False)  # 이미지
+    rental_val = db.Column(db.Integer, nullable=False)  # 총 대여 횟수
+    remaining = db.Column(db.Integer, nullable=False)  # 재고
 
 
+# 책 소개 페이지의 댓글 테이블
 class LibraryReview(db.Model):
-    # 책 소개 페이지 - 댓글 데이터
     __tablename__ = 'libraryReview'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_name = db.Column(db.String(255), nullable=False)
     user_email = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text(), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)  # 평가 별점
     book_id = db.Column(db.Integer, db.ForeignKey(
         'libraryBook.id'), nullable=False)
-    write_time = db.Column(db.DateTime, nullable=False)
-    user_email_code = db.Column(db.String(255), nullable=False)
+    write_time = db.Column(db.DateTime, default=datetime.utcnow())  # 작성 시간
+    # user_email_code = db.Column(db.String(255), nullable=False)  # 앞뒤로 **표시
 
-    def __init__(self, user_name, user_email, content, rating, book_id, write_time, user_email_code):
+    def __init__(self, user_name, user_email, content, rating, book_id, write_time):
         self.user_name = user_name
         self.user_email = user_email
         self.content = content
         self.rating = rating
         self.book_id = book_id
         self.write_time = write_time
-        self.user_email_code = user_email_code
+        # self.user_email_code = user_email_code
 
 
+# 책 대여 현황 테이블
 class RentalBook(db.Model):
-
     __tablename__ = 'rentalBook'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_email = db.Column(db.Integer, nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey(
         'libraryBook.id'), nullable=False)
-    rental_date = db.Column(db.Date, nullable=False)
+    rental_date = db.Column(db.Date, nullable=False)  # 대여 일자
+    return_date = db.Column(db.Date, nullable=False)  # 반납 일자
     book_data = db.relationship(
         'LibraryBook', foreign_keys='RentalBook.book_id')
 
--- views/main_view.py
+++ views/main_view.py
@@ -3,6 +3,7 @@ from models import *
 from db_connect import db
 from flask_bcrypt import Bcrypt
 # from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 bp = Blueprint('main', __name__, url_prefix='/')
 bcrypt = Bcrypt()
@@ -92,4 +93,101 @@ def logout():
     return redirect("/")
 
 
-# 대여하기
+# 책 전체 리스트 반환 - main.html
+@bp.route("/bookList")
+def book_list():
+    book_list = LibraryBook.query.order_by(LibraryBook.id.desc()).all()  # 내림차순
+
+    return render_template("main.html", book_list=book_list)
+
+
+# 책 소개 (상세) 페이지 - book_detail.html
+@bp.route('/bookDetail/<int:book_id>')
+def book_detail(book_id):
+    book_detail = LibraryBook.query.filter(LibraryBook.id == book_id).first()
+    review_list = LibraryReview.query.filter(
+        LibraryReview.book_id == book_id).order_by(LibraryReview.write_time.desc()).all()
+
+    return render_template('book_detail.html', book_detail=book_detail, review_list=review_list)
+
+
+# 책 소개(상세)의 댓글 - book_detail.html의 하단, form, input
+@bp.route("/writeReview/<int:book_id>", methods=['POST'])
+def write_review(book_id):
+    content = request.form['review']
+    rating = int(request.form['star'])
+    now = datetime.now()
+    write_time = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    review = LibraryReview(user_name=session['user_name'], user_email=session['user_email'],
+                           content=content, rating=rating, book_id=book_id, write_time=write_time)
+
+    db.session.add(review)
+    db.session.commit()
+
+    # return jsonify({"result": "success"})
+    return redirect(f'/bookDetail/{book_id}')
+
+
+# 댓글 수정 및 삭제 - book_detail.html의 하단
+
+
+# 대여 기록 반환 - rental_list.html
+@bp.route("/rentalList")
+def rental_list():
+
+    rental_list = RentalBook.query.filter(
+        RentalBook.user_email == session['user_email']).all()
+
+    return render_template('rental_list.html', rental_list=rental_list)
+
+
+# 대여하기 버튼을 눌렀을 때 동작하는 api : main.html
+@bp.route("/rentalBook", methods=['GET'])
+def rental_book():
+    book_id = request.args['book_id']
+
+    # 책의 재고가 0인 경우
+    book_info = LibraryBook.query.filter(LibraryBook.id == book_id).first()
+    if book_info.remaining == 0:
+        return jsonify({"result": "fail"})
+
+    book_info.remaining -= 1
+    book_info.rental_val += 1
+
+    # 이미 대여한 도서를 한번 더 클릭하는 경우
+    rental_books = RentalBook.query.filter(
+        RentalBook.user_email == session['user_email']).all()
+
+    for book in rental_books:
+        if book.book_id == int(book_id):
+            return jsonify({"result": "already"})
+
+    # 대여할 수 있는 권수를 초과한 경우
+    # if len(rental_books) >= 3:
+    #     return jsonify({"result" : "full"})
+
+    rental_info = RentalBook(
+        user_email=session['user_email'], book_id=book_id, rental_date=datetime.now())
+
+    db.session.add(rental_info)
+    db.session.commit()
+
+    return jsonify({"result": "success"})
+
+
+# 반납하기 - rental_list.html에서 반납하기 버튼 클릭
+@bp.route("/returnBook", methods=['GET'])
+def return_book():
+    book_id = request.args['book_id']
+
+    rental_book = RentalBook.query.filter(RentalBook.user_email == session['user_email']).filter(
+        RentalBook.book_id == book_id).first()
+    db.session.delete(rental_book)
+
+    book_info = LibraryBook.query.filter(LibraryBook.id == book_id).first()
+    book_info.remaining += 1
+
+    db.session.commit()
+
+    return jsonify({"result": "success"})
