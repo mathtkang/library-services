1d739a4c082aead7f99e271a40232448a7ef0b60
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-08-24 03:11:26 +0900

    feat : 기획 및 파일 생성

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-from flask import redirect, request, render_template, jsonify, Blueprint, Flask
+import pymysql
+from flask import Flask, render_template
+from db_connect import db
+from flask_bcrypt import Bcrypt
+import config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    bcrypt = Bcrypt(app)
+
+    # 블루프린트
+    from views import main_view
+    app.register_blueprint(main_view.bp)
+
+    # 세션 사용을 위해서
+    app.secret_key = "asdfasdfasdf"
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    return app
+
+
+if __name__ == "__main__":
+    create_app().run(debug=True, port=5000)
