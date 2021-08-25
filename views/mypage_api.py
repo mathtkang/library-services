c1730a153744a0168e44c9939b0e08bc315586f8
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-08-25 19:27:14 +0900

    feat : 마이페이지 html, api 구현

--- views/mypage_api.py
+++ views/mypage_api.py
@@ -5,23 +5,34 @@ from datetime import datetime
 
 bp = Blueprint('mypage', __name__, url_prefix='/')
 
+'''
+연결 html : mypage.html
+1. GET : 대여 기록 반환
+2. POST : '반납하기' 버튼 클릭시 대여목록에서 지우고, 책 반납하기
+:param book_id:
+:return:
+'''
+UserRentBook
 
-# 대여 기록 반환 - mypage.html
-@bp.route("/rentalList")
+
+@bp.route("/rentalList", methods=['GET', 'POST'])
 def rental_list():
+    rental_list = UserRentBook.query.filter(
+        UserRentBook.user_email == session['user_email']).all()
+    # rents = UserBookRent.query.filter(UserBookRent.user_email == current_user.id)
 
-    rental_list = RentalBook.query.filter(
-        RentalBook.user_email == session['user_email']).all()
+    if request.method == 'POST':
+        pass
 
-    return render_template('rental_list.html', rental_list=rental_list)
+    return render_template('mypage.html', rental_list=rental_list)
 
 
-# 반납하기 - mypage.html(하단)에서 반납하기 버튼 클릭
+# 반납하기 - mypage.html(하단)에서 반납하기 버튼 클릭 - post로 받아오기
 @ bp.route("/returnBook", methods=['GET'])
 def return_book():
     book_id = request.args.get('book_id')
-    rental_book = RentalBook.query.filter(RentalBook.user_email == session['user_email']).filter(
-        RentalBook.book_id == book_id).first()
+    rental_book = UserRentBook.query.filter(UserRentBook.user_email == session['user_email']).filter(
+        UserRentBook.book_id == book_id).first()
 
     db.session.delete(rental_book)
 
--- views/return_book_api.py
+++
@@ -1,6 +0,0 @@
-from flask import redirect, request, render_template, jsonify, Blueprint, session, g, url_for
-from models import *
-# from flask_bcrypt import Bcrypt
-from datetime import datetime
-
-bp = Blueprint('return_book', __name__, url_prefix='/')
